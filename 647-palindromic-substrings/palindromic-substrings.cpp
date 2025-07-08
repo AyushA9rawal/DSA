c1e7bcb3742364a4c69aecc0aeabe38dd417860b
Ayush S. Agrawal <134122917+AyushA9rawal@users.noreply.github.com>
2025-07-08 09:35:40 +0530

    Time: 39 ms (24.09%) | Memory: 13 MB (41.77%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+    int t[1001][1001];
     bool solve(int i,int j,string &s)
     {
         if(i>=j) 
         {
-            return true;
+            return 1;
         }
+        if(t[i][j]!=-1) return t[i][j];
         if(s[i]==s[j])
         {
-            return solve(i+1,j-1,s);
+            return t[i][j]=solve(i+1,j-1,s);
         }
         else
         {
-            return false;
+            return 0;
         }
     }
     int countSubstrings(string s) {
         int count=0;
+        memset(t,-1,sizeof(t));
         for(int i=0;i<s.size();i++)
         {
             for(int j=i;j<s.size();j++)
