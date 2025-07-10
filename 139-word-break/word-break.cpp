6178150b962d522eb4eef04f3aa992dd8fec3516
Ayush S. Agrawal <134122917+AyushA9rawal@users.noreply.github.com>
2025-07-10 09:24:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (86.51%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,26 +1,51 @@
 class Solution {
 public:
-    int t[301];
-    bool wordBreak(string s, vector<string>& wordDict) {
-        memset(t,-1,sizeof(t));
-        return dfs(s,wordDict,0);
-    }
+    // int t[301];
+    // bool wordBreak(string s, vector<string>& wordDict) {
+    //     memset(t,-1,sizeof(t));
+    //     return dfs(s,wordDict,0);
+    // }
 
-    bool dfs(string &s,vector<string>&wordDict,int i)
-    {
-        if(i==s.length()) return true;
-        if(t[i]!=-1) return t[i];
+    // bool dfs(string &s,vector<string>&wordDict,int i)
+    // {
+    //     if(i==s.length()) return true;
+    //     if(t[i]!=-1) return t[i];
+    //     for(string word:wordDict)
+    //     {
+    //         if( s.substr(i,word.length())==word && i+word.length()<=s.length())
+    //         {
+    //             if(dfs(s,wordDict,i+word.length()))
+    //             {
+    //                 return t[i]=1;
+    //             }
+    //         }
+    //     }
+    //     return t[i]=0;
+    // }
+
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string>myset(wordDict.begin(),wordDict.end());
+        int maxlen=0;
         for(string word:wordDict)
         {
-            if( s.substr(i,word.length())==word && i+word.length()<=s.length())
+            maxlen=max(maxlen,(int)word.size());
+        }
+        int n=s.size();
+        vector<bool>dp(n+1);
+        dp[0]=true;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=i-1;j>=max(0,i-maxlen);j--)
             {
-                if(dfs(s,wordDict,i+word.length()))
+                if(dp[j] && myset.find(s.substr(j,i-j))!=myset.end())
                 {
-                    return t[i]=1;
+                    dp[i]=true;
+                    break;
                 }
             }
         }
-        return t[i]=0;
-    }
 
+        return dp[n];
+    }
 };
