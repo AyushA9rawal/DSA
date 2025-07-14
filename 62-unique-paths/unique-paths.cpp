0b112193a739edcad799acf4282b1e0c1f57a501
Ayush S. Agrawal <134122917+AyushA9rawal@users.noreply.github.com>
2025-07-14 15:02:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (96.61%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>>memo;
+    // vector<vector<int>>memo;
     int uniquePaths(int m, int n) {
-        memo.resize(m,vector<int>(n,-1));
-        return func(m,n,0,0);
-    }
-
-    int func(int m,int n,int i,int j)
-    {
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
-        if(i>=m || j>=n ){
-            return 0;
-        }
-        if(memo[i][j]!=-1)
+        // memo.resize(m,vector<int>(n,-1));
+        // return func(m,n,0,0);
+        int num=m+n-2;
+        int k=min(m-1,n-1);
+        long long res=1;
+        for(int i=1;i<=k;i++)
         {
-            return memo[i][j];
+            res*=(num-i+1);
+            res/=i;
         }
-
-        return memo[i][j]=func(m,n,i,j+1)+func(m,n,i+1,j);
-        
-        
+        return res;
     }
+
+    // int func(int m,int n,int i,int j)
+    // {
+    //     if(i==m-1 && j==n-1){
+    //         return 1;
+    //     }
+    //     if(i>=m || j>=n ){
+    //         return 0;
+    //     }
+    //     if(memo[i][j]!=-1)
+    //     {
+    //         return memo[i][j];
+    //     }
+
+    //     return memo[i][j]=func(m,n,i,j+1)+func(m,n,i+1,j);
+        
+        
+    // }
 };
