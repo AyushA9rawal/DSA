15b21008d4cc6a9c62ccba2155a6405672a51a9a
Ayush S. Agrawal <134122917+AyushA9rawal@users.noreply.github.com>
2025-07-17 21:48:42 +0530

    Time: 170 ms (34.08%) | Memory: 60.2 MB (11.23%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    unordered_map<int,int> t[21];
     int solve(vector<int>&nums,int i,int sum)
     {
         if(i>=nums.size())
@@ -10,14 +11,18 @@ public:
             }
             return 0;
         }
+
+        if(t[i].count(sum))
+        {
+            return t[i][sum];
+        }
         
 
         int plus=solve(nums,i+1,sum-nums[i]);
         int minus=solve(nums,i+1,sum+nums[i]);
-        return plus + minus;
+        return t[i][sum]=plus + minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        
         return solve(nums,0,target);
     }
 };
